336a2aaf251aa8047fea267c72674912336499cd
Bryan Leboff <bleboff@appirio.com>
2018-12-28 12:19:59 -0500

    finish up configuration and files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const verifyConditions = require('./lib/verify')
-const publish = require('./lib/publish')
+const prepare = require('./lib/prepare')
 
 module.exports = {
   verifyConditions,
-  publish,
+  prepare,
 }
